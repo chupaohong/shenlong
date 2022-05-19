22a2c06768f2c1ac42e69a71d66fce8cc8005141
phitb <phitb@localhost>
2022-05-19 02:50:09 +0000

    CVE-2022-28346

---
+++ demo/views.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db.models import Count
+from django.http import HttpResponse
+
+# Create your views here.
+from django.shortcuts import render
+
+from demo.models import User
+
+
+def loadexampledata(request):
+    u = User(name="Admin")
+    u.save()
+    u = User(name="Staff1")
+    u.save()
+    u = User(name="Staff12")
+    u.save()
+    return render(request, 'index.html')
+
+
+def users(request):
+    field = request.GET.get('field', 'name')
+    user_amount = User.objects.annotate(**{field: Count("name")})
+    html = ""
+    for u in user_amount:
+        html += "<h3>Amoount of users: {0}</h3>".format(u)
+    return HttpResponse(html)
